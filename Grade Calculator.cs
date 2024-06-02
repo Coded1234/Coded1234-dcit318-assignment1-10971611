2d632673589da1c23bce62eeb37d08a0c68524a9
DEKU WILSON MAKAFUI <102229517+Coded1234@users.noreply.github.com>
2024-06-02 23:08:14 +0000

    Add files via upload
    .

---
+++ Grade Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DCIT318Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeCalculator();
+            Console.ReadLine();
+        }
+
+        static void GradeCalculator()
+        {
+            Console.Write("Enter a numerical score: ");
+            int grade = int.Parse(Console.ReadLine());
+
+            string letterGrade;
+
+            if (grade >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (grade >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (grade >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (grade >= 60)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+
+            Console.WriteLine($"Your letter grade is: {letterGrade}");
+        }
+    }
+}
